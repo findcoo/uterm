1557cfde070d3d78cf8812f5a1ff6aa9640dd36b
findcoo <thirdlif2@gmail.com>
2018-03-01 16:28:00 +0900

    Add Urls entity type

--- src/main/java/com/uterm/domain/Urls.java
+++ src/main/java/com/uterm/domain/Urls.java
@@ -1,17 +1,28 @@
 package com.uterm.domain;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import lombok.Data;
 
 @Data
 @Entity
 public class Urls {
-    private @Id @GeneratedValue Long id;
+    @Id
+    @GeneratedValue
+    private Long id;
     private String domain;
     private String urlB62;
+    @CreatedDate
+    private LocalDateTime CreatedAt;
+    @LastModifiedDate
+    private LocalDateTime ModifiedAt;
 
     private Urls(String domain, String url) {
         this.domain = domain;
