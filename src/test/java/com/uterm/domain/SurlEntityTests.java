a7efcc87d92f7243f36542aef60cf14fe5dda498
findcoo <thirdlif2@gmail.com>
2018-03-02 14:19:03 +0900

    Add h2 dependencies

--- src/test/java/com/uterm/domain/SurlEntityTests.java
+++ src/test/java/com/uterm/domain/SurlEntityTests.java
@@ -10,7 +10,6 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -20,7 +19,6 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace=Replace.NONE)
 public class SurlEntityTests {
    
     @Rule
